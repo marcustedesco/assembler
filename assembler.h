19d7e67156d8f383843ec8de811c4856ec38185a
marcustedesco <mtedesco@vt.edu>
2013-11-18 17:28:05 -0500

    new file:   assembler.c 	new file:   assembler.h 	new file:   driver.c

---
+++ assembler.h
@@ -0,0 +1,22 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char name[128];
+	int index;
+	//0 if in text array, 1 if in data array
+	int TorD;
+} Symbol;
+
+void run (char * inputFile, char * outFile);
+char* cleanStr(char line[]);
+char * removeComment(char line[]);
+int getRegNum(char reg[]);
+void executeCommands(int *reg, char commands[128][128], int usage);
+void printBinary(int *reg, char commands[128][128], int usage, char * outFile);
+char * intToBinChar(int num, int length);
+char * compareBin(char first[], char second[], int instruc);
+
+#endif
